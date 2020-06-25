149bb9692b7794703952845f1a83cf26c5ecf03d
kabish01 <kabiswor@gmail.com>
2020-06-26 01:05:25 +0545

    Update facto.py

--- facto.py
+++ facto.py
@@ -1,14 +1,4 @@
-# num = 10
-# factorial = 1
-# summ = 0
-# for i in range(1, num+1):
-#     factorial = factorial *i 
-    
-    
-#     summ += int(factorial%10) 
-#     n = int(factorial/10) 
-#     print(summ)
-  
+ 
 def getSum(num): 
     
     factorial = 1
