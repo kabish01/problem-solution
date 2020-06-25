48530c6cc667cff668cf6ca62cb6ecb6a310f611
kabish01 <kabiswor@gmail.com>
2020-06-26 01:03:14 +0545

    Add files via upload

---
+++ facto.py
@@ -0,0 +1,31 @@
+# num = 10
+# factorial = 1
+# summ = 0
+# for i in range(1, num+1):
+#     factorial = factorial *i 
+    
+    
+#     summ += int(factorial%10) 
+#     n = int(factorial/10) 
+#     print(summ)
+  
+def getSum(num): 
+    
+    factorial = 1
+    summ = 0
+    for i in range(1, num+1):
+        factorial = factorial *i 
+    print(factorial)
+  
+    # Single line that calculates sum  
+    while(factorial > 0): 
+        summ += int(factorial%10) 
+        factorial = int(factorial/10) 
+  
+    return summ
+ 
+# Driver code 
+num = 100
+# n = 687
+print(getSum(num))
+   
